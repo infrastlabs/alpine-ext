4f7d727f4cd8362e1ad3ce7dfb6cd2de371dfca0
sam <huapox@126.com>
2020-03-19 23:09:32 +0800

    ns=infrastlabs

--- build_img.sh
+++ build_img.sh
@@ -7,8 +7,8 @@ source ~/.auth-docker-registry #set DOCKER_REGISTRY_PW DOCKER_REGISTRY_USER
 #export |grep DOCKER_REG
 echo "${DOCKER_REGISTRY_PW}" |docker login --username=${DOCKER_REGISTRY_USER} --password-stdin $repo
 
-ns=k-pub
-ver=v1.0 #latest
+ns=infrastlabs
+ver=latest #v1.0
 cur=$(cd "$(dirname "$0")"; pwd) 
 
 #bin
