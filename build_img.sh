9e508d90b005f0e1518b41e0c2e15346374899bc
sam <huapox@126.com>
2020-03-22 12:53:28 +0800

    build weak/gosu

--- build_img.sh
+++ build_img.sh
@@ -5,15 +5,22 @@
 repo=registry.cn-shenzhen.aliyuncs.com
 source ~/.auth-docker-registry #set DOCKER_REGISTRY_PW DOCKER_REGISTRY_USER
 #export |grep DOCKER_REG
-echo "${DOCKER_REGISTRY_PW}" |docker login --username=${DOCKER_REGISTRY_USER} --password-stdin $repo
+echo "${DOCKER_REGISTRY_PW_sldevdir}" |docker login --username=${DOCKER_REGISTRY_USER_sldevdir} --password-stdin $repo
 
 ns=infrastlabs
 ver=latest #v1.0
 cur=$(cd "$(dirname "$0")"; pwd) 
 
 #bin
-parent=$repo/$ns/alpine-ext-bin:$ver
+parent=$repo/$ns/alpine-ext:bin
 #docker build --pull -t $parent $cur/bin/.
+
 #src
-docker build --pull -t $repo/$ns/alpine-ext:$ver --build-arg parent=$parent  $cur/src/. 
-docker push $repo/$ns/alpine-ext:$ver
+# docker build --pull -t $repo/$ns/alpine-ext:$ver --build-arg parent=$parent $cur/src/. 
+# docker push $repo/$ns/alpine-ext:$ver
+
+#example
+docker build -t $repo/$ns/alpine-ext:gosu $cur/example-gosu/.
+docker push $repo/$ns/alpine-ext:gosu
+docker build -t $repo/$ns/alpine-ext:weak $cur/example-weak/.
+docker push $repo/$ns/alpine-ext:weak
