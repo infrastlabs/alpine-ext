c14a4cc7d510f1c4d2ea6fe09f69d51d6958509b
sam <huapox@126.com>
2020-03-22 13:13:26 +0800

    up2

--- build_img.sh
+++ build_img.sh
@@ -8,18 +8,19 @@ source ~/.auth-docker-registry #set DOCKER_REGISTRY_PW DOCKER_REGISTRY_USER
 echo "${DOCKER_REGISTRY_PW_sldevdir}" |docker login --username=${DOCKER_REGISTRY_USER_sldevdir} --password-stdin $repo
 
 ns=infrastlabs
-ver=latest #v1.0
+ver=bin
 cur=$(cd "$(dirname "$0")"; pwd) 
 
 #bin
-parent=$repo/$ns/alpine-ext:bin
+parent=$repo/$ns/alpine-ext:$ver
 #docker build --pull -t $parent $cur/bin/.
 
 #src
+ver=latest #v1.0
 # docker build --pull -t $repo/$ns/alpine-ext:$ver --build-arg parent=$parent $cur/src/. 
 # docker push $repo/$ns/alpine-ext:$ver
 
-#example
+#example-gosu/weak
 docker build -t $repo/$ns/alpine-ext:gosu $cur/example-gosu/.
 docker push $repo/$ns/alpine-ext:gosu
 docker build -t $repo/$ns/alpine-ext:weak $cur/example-weak/.
